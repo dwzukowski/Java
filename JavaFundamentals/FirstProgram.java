59d6aa08eaf1a4056b5b0516197f1cc96dd5cddb
dwzukowski <dwzukowski@gmail.com>
2017-07-03 16:39:10 -0400

    first Java code

---
+++ JavaFundamentals/FirstProgram.java
@@ -0,0 +1,10 @@
+public class FirstProgram{
+    public static void main(String[] args){
+        String name = "My name is coding dojo";
+        String age = "I am 100 years old";
+        String location = "My hometown in Burbank, CA";
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(location);
+    }
+}
