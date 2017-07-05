9f815a8a9a21981145afe8630466b6ab4dca632e
dwzukowski <dwzukowski@gmail.com>
2017-07-05 08:54:04 -0400

    fizzbuzz

---
+++ JavaFundamentals/FizzBuzz/FizzBuzzTest.java
@@ -0,0 +1,7 @@
+class FizzBuzzTest{
+    public static void main(String[] args){
+        FizzBuzz result = new FizzBuzz();
+        String output = result.fizzBuzz(15);
+        System.out.println(output);
+    }
+}
